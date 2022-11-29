5e739d6c364ba6668e10e22578bffcee7cb3e593
Edward <ak0420@mau.se>
2022-11-29 13:10:30 +0100

    created classes and README

--- Chess.cs
+++ Chess.cs
@@ -8,5 +8,6 @@ namespace MiniGames
 {
     internal class Chess
     {
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Booyah!");
+            Console.WriteLine("");
+            Console.ReadLine();
         }
     }
 }

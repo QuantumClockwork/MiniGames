b4b409a7c8d767525d22b970b11478e36728ee19
Edward <ak0420@mau.se>
2022-12-03 02:32:44 +0100

    Work on tic tac toe and keyboard input

--- Program.cs
+++ Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            TicTacToe ticTacToeGame = new TicTacToe();
+            ticTacToeGame.Game();
             Console.ReadLine();
+            //reference kinda
+            string gameBoard = "   a     b     c" +
+                           "\n      |     |     " +
+                           "\n1  -  |  -  |  -  " +
+                           "\n _____|_____|_____" +
+                           "\n      |     |     " +
+                           "\n2  -  |  -  |  -  " +
+                           "\n _____|_____|_____" +
+                           "\n      |     |     " +
+                           "\n3  -  |  -  |  -  " +
+                           "\n      |     |     ";
         }
     }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -8,6 +8,72 @@ namespace MiniGames
 {
     internal class TicTacToe
     {
+        bool isNavigation;
+        string[,] gameBoardArr = new string[3, 3];
+        string player1Token, player2Token, gameBoard;
 
+        public TicTacToe()
+        {
+            /// what does this object need?
+            /// it needs a board that it draws
+            /// it need to have X and O symbols (tokens)
+            /// it needs methods that alter the board after input
+
+            isNavigation = true;
+            this.player1Token = "X";
+            this.player2Token = "O";
+
+            for (int i = 0; i < gameBoardArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameBoardArr.GetLength(1); j++)
+                {
+                    gameBoardArr[i, j] = "-";
+                }
+            }
+        }
+
+        public void Game()
+        {
+            GameLoop();
+        }
+
+        public void GameLoop()
+        {
+            // array visual
+            // 0 1 2
+            // 3 4 5
+            // 6 7 8
+
+            ConsoleKeyInfo keyInputInfo;
+
+            while (isNavigation)
+            {
+                keyInputInfo = Console.ReadKey();
+
+                if (keyInputInfo.Key == ConsoleKey.UpArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("UP");
+                }
+
+                if (keyInputInfo.Key == ConsoleKey.DownArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("DOWN");
+                }
+
+                if (keyInputInfo.Key == ConsoleKey.RightArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("RIGHT");
+                }
+
+                if (keyInputInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("LEFT");
+                }
+            }
+        }
     }
 }

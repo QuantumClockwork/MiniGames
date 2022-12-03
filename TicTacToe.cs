f75e966422f265e093f35cb133b820570be8a499
Edward <ak0420@mau.se>
2022-12-03 16:30:07 +0100

    working out the draw for the board with the array inside it

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -9,26 +9,18 @@ namespace MiniGames
     internal class TicTacToe
     {
         bool isNavigation;
-        string[,] gameBoardArr = new string[3, 3];
-        string player1Token, player2Token, gameBoard;
+        string[] arr = new string[9];
+        string player1Token, player2Token;
 
         public TicTacToe()
         {
-            /// what does this object need?
-            /// it needs a board that it draws
-            /// it need to have X and O symbols (tokens)
-            /// it needs methods that alter the board after input
-
             isNavigation = true;
             this.player1Token = "X";
             this.player2Token = "O";
 
-            for (int i = 0; i < gameBoardArr.GetLength(0); i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < gameBoardArr.GetLength(1); j++)
-                {
-                    gameBoardArr[i, j] = "-";
-                }
+                arr[i] = "-";
             }
         }
 
@@ -39,21 +31,33 @@ namespace MiniGames
 
         public void GameLoop()
         {
-            // array visual
-            // 0 1 2
-            // 3 4 5
-            // 6 7 8
-
+            arr[4] = "X";
             ConsoleKeyInfo keyInputInfo;
 
             while (isNavigation)
             {
+                Console.WriteLine("___________________" +
+                           "\n|     |     |     |" +
+                           "\n|  " + arr[0] + "  |  " + arr[1] + "  |  " + arr[2] + "  |" +
+                           "\n|_____|_____|_____|" +
+                           "\n|     |     |     |" +
+                           "\n|  " + arr[3] + "  |  " + arr[4] + "  |  " + arr[5] + "  |" +
+                           "\n|_____|_____|_____|" +
+                           "\n|     |     |     |" +
+                           "\n|  " + arr[6] + "  |  " + arr[7] + "  |  " + arr[8] + "  |" +
+                           "\n|_____|_____|_____|");
+
                 keyInputInfo = Console.ReadKey();
 
                 if (keyInputInfo.Key == ConsoleKey.UpArrow)
                 {
                     Console.Clear();
-                    Console.WriteLine("UP");
+                    
+                    // need if statements that make sure movement on array is done the right way
+
+                    // scripted movement, not actually proper
+                    arr[4] = "-";
+                    arr[1] = "X";
                 }
 
                 if (keyInputInfo.Key == ConsoleKey.DownArrow)
